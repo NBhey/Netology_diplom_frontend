f818be3fc833b4fe97ca177adafa08710b241225
Oganes <og.marckaryan@yandex.ru>
2025-05-15 20:34:59 +0300

    15.05.2025: добавил календарь

---
+++ src/components/Calendar/Calendar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Calendar.css"; // Стили для компонента
+
+const Calendar = () => {
+  const currentDate = new Date();
+  const daysOfWeek = ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"];
+
+  const getDatesToShow = () => {
+    const dates = [];
+    for (let i = 0; i < 6; i++) {
+      const date = new Date();
+      date.setDate(currentDate.getDate() + i);
+      dates.push(date);
+    }
+
+    return dates;
+  };
+
+  const dates = getDatesToShow();
+  console.log(dates);
+  return (
+    <div className="calendar-container">
+      <div className="calendar-header">
+        {dates.map((date, index) => (
+          <div key={index} className={`day-cell ${index === 0 ? 'today' : ''}`}>
+            <div className="day-name">{daysOfWeek[date.getDay()]}</div>
+            <div className="day-number">{date.getDate()}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Calendar;
